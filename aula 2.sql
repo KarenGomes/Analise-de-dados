c4333262dc59634555b91e09f7844878416e8390
Karen Gomes <karenecris12015@gmail.com>
2023-11-14 15:10:42 -0300

    aula 3 e 5

--- aula 2.sql
+++ aula 2.sql
@@ -1,25 +1,29 @@
-select * from order_details 
+select product_name, 
+	quantity_per_unit, 
+	unit_price, 
+	category_name, 
+	description
+from products 
+inner join categories on 
+categories.category_id = products.category_id
 
-select sum(quantity) as total_unit_sold
+select *
 from order_details 
+where quantity > 
+	(
+	-- Subquery
+	select avg(quantity)
+	from order_details 
+	)
 
-select product_id, sum(quantity) as total_units_sold
-from order_details  
-group by product_id
-order by total_units_sold
+--- CTE -> common table expression
 
-select date_trunc('month', order_date) as order_month, count(order_id)
-from orders
-group by order_month
-order by order_month
+with average as (
+	select avg(quantity) as average_quantity
+	from order_details 
+)
 
-select * from order_details
-
-select product_id, sum(quantity) as total_units_sold 
-from order_details
-group by product_id
-having sum(quantity) >= 1000
-
-
-
-
+select *
+from order_details, average
+where quantity > average.average_quantity
+	
